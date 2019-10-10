ad7e75b8553b00be32a65bc54878f23c82728290
qikiqi <11520039+qikiqi@users.noreply.github.com>
2019-10-10 21:47:22 +0300

    Include loop

--- test_cluster_response_time.py
+++ test_cluster_response_time.py
@@ -2,13 +2,16 @@ import requests
 import pprint as pp
 import sys
 
+times = int(sys.argv[2])
 
-headers = {
-    'Content-Type': 'application/json',
-}
+for i in range(times):
+    headers = {
+        'Content-Type': 'application/json',
+    }
 
-data = '{"profile": true,"query" : {"match" : { "ts_date" : "20180315" }}}'
+    data = '{"profile": true,"query" : {"match" : { "ts_date" : "20180315" }}}'
 
-response = requests.post('http://elastic:changeme@{}:9200/filebeat/_search?human=true'.format(sys.argv[1]), headers=headers, data=data)
+    response = requests.post('http://elastic:changeme@{}:9200/filebeat/_search?human=true'.format(sys.argv[1]), headers=headers, data=data)
 
-pp.pprint(response.json()['profile']['shards'][0]['searches'][0]['query'][0]['time'])
+    resp_time = response.json()['profile']['shards'][0]['searches'][0]['query'][0]['time']
+    print("Iteration {} response time: {}".format(i, resp_time))
