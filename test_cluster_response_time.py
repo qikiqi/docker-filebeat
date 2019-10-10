190ca749b882aa9ceec975ddb066b77e66686b4f
qikiqi <11520039+qikiqi@users.noreply.github.com>
2019-10-10 21:38:45 +0300

    Parse only the response time

--- test_cluster_response_time.py
+++ test_cluster_response_time.py
@@ -9,6 +9,6 @@ headers = {
 
 data = '{"profile": true,"query" : {"match" : { "ts_date" : "20180315" }}}'
 
-response = requests.post('http://{}:9200/filebeat/_search?human=true'.format(sys.argv[1]), headers=headers, data=data)
+response = requests.post('http://elastic:changeme@{}:9200/filebeat/_search?human=true'.format(sys.argv[1]), headers=headers, data=data)
 
-pp.pprint(response.json())
+pp.pprint(response.json()['profile']['shards'][0]['searches'][0]['query'][0]['time'])
